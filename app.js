d88b3a92e4cfe4286c8729f6d3b87a88b0479920
Arnaud lewis <arnaud.lewis@gmail.com>
2017-06-02 13:54:16 +0200

    update template to simplify syntax and use Link Helper from prismic-dom

--- app.js
+++ app.js
@@ -18,15 +18,16 @@ app.use((req, res, next) => {
   Prismic.api(PrismicConfig.apiEndpoint)
   .then((api) => {
     req.prismic = { api };
-    res.locals.DOM = PrismicDOM;
+    res.locals.RichText = PrismicDOM.RichText;
+    res.locals.Link = PrismicDOM.Link;
+    res.locals.Date = PrismicDOM.Date;
     res.locals.ctx = {
       endpoint: PrismicConfig.apiEndpoint,
       linkResolver: PrismicConfig.linkResolver,
     };
     next();
   }).catch((err) => {
-    const message = err.status === 404 ? 'There was a problem connecting to your API, please check your configuration file for errors.' : `Error 500: ${err.message}`;
-    res.status(err.status).send(message);
+    next(err);
   });
 });
 
@@ -45,7 +46,7 @@ app.get('/page/:uid', (req, res) => {
   });
 });
 
-app.get('/preview', (req, res) => {
+app.get('/preview', (req, res, next) => {
   const token = req.query.token;
   if (token) {
     req.prismic.api.previewSession(token, PrismicConfig.linkResolver, '/').then((url) => {
@@ -53,7 +54,7 @@ app.get('/preview', (req, res) => {
       cookies.set(Prismic.previewCookie, token, { maxAge: 30 * 60 * 1000, path: '/', httpOnly: false });
       res.redirect(301, url);
     }).catch((err) => {
-      res.status(500).send(`Error 500 in preview: ${err.message}`);
+      next(err);
     });
   } else {
     res.send(400, 'Missing token from querystring');
